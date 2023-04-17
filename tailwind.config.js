7d3ebe075052232e1dd15390c6c3de11a12c9938
sam-c14 <samuelaffah14@gmail.com>
2023-04-17 08:29:26 +0100

    Default breakpoints in tailwind overwitten

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      lg: "1280px",
+      // => @media (min-width: 1440px) { ... }
+      xl: "1440px",
+      // => @media (min-width: 1440px) { ... }
+      xxl: "1920px",
+      // => @media (min-width: 1920px) { ... }
+    },
     extend: {},
   },
   plugins: [],
