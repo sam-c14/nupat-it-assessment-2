86942a45c61258928b9dff3a8122170510ba9fe2
sam-c14 <samuelaffah14@gmail.com>
2023-05-01 22:16:05 +0100

    Project made responsive

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -30,32 +30,32 @@ const HomeHeader = () => {
       className="font-inter bg-white pb-4 pt-11 lg:pl-0 pl-8 pr-8 shadow-alt_2"
       id="home-component"
     >
-      <div className="w-full lg:px-8 xl:px-10 flex md:justify-between md:gap-0 gap-y-3 flex-wrap">
-        <div className="md:w-auto w-full">
-          <h1 className="capitalize leading-10 font-semibold text-4xl">
+      <div className="w-full lg:px-8 xl:px-10 flex sm:justify-between md:gap-0 gap-y-3 flex-wrap">
+        <div className="sm:w-auto w-full">
+          <h1 className="capitalize leading-10 font-semibold text-3xl md:text-4xl">
             new campaign run
           </h1>
           <p className="opacity-50 xxl:mt-0 mt-3.5 xl:mt-2 md:text-sm text-xs xxl:text-base">
             A new campaign launch work for brand new featur in May month
           </p>
         </div>
-        <div className="sm:w-auto w-full">
-          <button className="flex gap-3 mb-2 items-center px-8 py-2.5 rounded-lg text-sm uppercase text-white bg-black">
+        <div className="sm:w-auto lg:justify-normal lg:block sm:flex sm:items-end pt-2 pb-0 w-full">
+          <button className="flex gap-3 mb-2 items-center px-8 py-2.5 lg:h-auto sm:h-2/3 rounded-lg text-sm uppercase text-white bg-black">
             add members
           </button>
         </div>
       </div>
-      <div className="nav lg:px-8  xl:px-10 mt-10 flex md:justify-between flex-wrap items-center">
+      <div className="nav lg:px-8 xl:px-10 mt-5 md:mb-0 mb-2 md:mt-10 flex md:justify-between flex-wrap items-center">
         <div className="flex gap-2 w-full md:w-1/5 items-center">
           <img src={ProfileStack} alt="profile-stack" />
-          <p className="opacity-50 text-sm">8 members</p>
+          <p className="opacity-50 whitespace-nowrap text-sm">8 members</p>
         </div>
-        <div className="xl:w-1/2 xxl:w-1/3 md:w-auto w-full lg:w-3/5 flex items-center">
-          <ul className="md:flex grid grid-cols-2 w-full pt-2 justify-between">
+        <div className="xl:w-1/2 xxl:w-1/3 md:w-auto w-full lg:w-3/5 mt-1 flex items-center">
+          <ul className="md:flex grid sm:grid-cols-3 grid-cols-2 gap-y-2 md:gap-x-2 w-full pt-2 justify-between">
             {links.map((link) => (
               <li
                 key={link.text}
-                className="flex md:text-sm text-xs cursor-pointer gap-3 mb-1"
+                className="flex md:text-sm text-xs cursor-pointer gap-2.5 lg:gap-3 mb-1"
               >
                 <img src={link.icon} alt="icon" />
                 {link.text}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HalfClock from "../assets/images/HalfClock.svg";
 import Fan from "../assets/images/Fan.svg";
 import Bolt from "../assets/images/Bolt.svg";
@@ -45,19 +45,42 @@ const Sidebar = () => {
       text: "Logout",
     },
   ];
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const showMenu = () => {
+    const menu = document.querySelector(".menu");
+    const burger = document.getElementById("burger");
+    // console.log(menu, isMenuOpen);
+    if (isMenuOpen) {
+      // console.log("Menu is Showing");
+      menu.classList.remove("show-top");
+      menu.classList.add("hide-top");
+      burger.style.borderRadius = "0px";
+      burger.style.backgroundColor = "transparent";
+      burger.style.padding = "0px";
+      document.documentElement.style.overflow = "auto";
+    } else {
+      menu.classList.remove("hide-top");
+      menu.classList.add("show-top");
+      burger.style.borderRadius = "100px";
+      burger.style.backgroundColor = "white";
+      burger.style.padding = "5px";
+      document.documentElement.style.overflow = "hidden";
+    }
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div>
-      <div className="h-screen hidden lg:flex w-68 py-8 flex-wrap justify-center absolute font-inter text-white bg-primary">
+      <div className="h-screen menu flex w-full lg:w-68 py-8 lg:top-auto -top-full flex-wrap transition-smooth justify-center lg:absolute fixed font-inter text-white bg-primary">
         <div className="mb-4">
           <button className="flex gap-3 mb-5 items-center px-8 py-2.5 rounded-md bg-btn_primary">
             <img src={Bolt} /> Welcome Keerth!
           </button>
           <div className="list mt-7">
-            <ul>
+            <ul className="lg:block w-full grid grid-cols-1">
               {links.map((link) => (
                 <li
                   key={link.text}
-                  className="flex text-sm gap-3 cursor-pointer mb-7"
+                  className="flex text-xs md:text-sm gap-3 cursor-pointer mb-7"
                 >
                   <img src={link.icon} alt="icon" />
                   {link.text}
@@ -65,11 +88,11 @@ const Sidebar = () => {
               ))}
             </ul>
             <hr className="h-0 min-w-0" />
-            <ul className="mt-8">
+            <ul className="mt-8 lg:block w-full grid md:grid-cols-1 grid-cols-2">
               {endLinks.map((link) => (
                 <li
                   key={link.text}
-                  className="flex text-sm gap-3 cursor-pointer mb-7 opacity-60"
+                  className="flex text-xs md:text-sm gap-3 cursor-pointer mb-7 opacity-60"
                 >
                   <img src={link.icon} alt="icon" />
                   {link.text}
@@ -78,13 +101,17 @@ const Sidebar = () => {
             </ul>
           </div>
         </div>
-        <div className="flex items-end">
+        <div className="flex lg:w-auto sm:w-full sm:justify-center lg:justify-normal items-end">
           <button className="flex gap-3 mb-5 items-center px-8 py-3 font-normal uppercase rounded-md bg-btn_secondary">
             <img src={Plus} /> new project
           </button>
         </div>
       </div>
-      <div className="lg:hidden w-5 absolute top-10">
+      <div
+        onClick={showMenu}
+        className="lg:hidden w-5 sm:right-10 right-5 absolute top-14 sm:top-9"
+        id="burger"
+      >
         <img src={Burger} alt="hamburger" />
       </div>
     </div>
